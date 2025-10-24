07158ec1fd11c5da86670272f9d9b4ea2c8c8173
CatHat <liase810@gmail.com>
2025-10-24 15:06:50 +0200

    Fix player movement script + Adjust node positions

--- scripts/player.gd
+++ scripts/player.gd
@@ -9,25 +9,17 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	#if not is_on_floor():
 	#	velocity += get_gravity() * delta
-
 	# Handle jump.
 	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 	#	velocity.y = JUMP_VELOCITY
-
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction1 := Input.get_axis("ui_left", "ui_right")
-	var direction2 := Input.get_axis("ui_up", "ui_down")
+	var direction_x := Input.get_axis("ui_left", "ui_right")
+	var direction_y := Input.get_axis("ui_up", "ui_down")
 	
-	if direction1:
-		velocity.x = direction1 * SPEED
-	elif direction2:
-		velocity.y = direction2 * SPEED
-	elif direction1 and direction2:
-		velocity.x = direction1 * SPEED
-		velocity.y = direction2 * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
+	var targetspeed = {x = direction_x * SPEED, y = direction_y * SPEED}
+	
+	velocity.x = move_toward(velocity.x, targetspeed.x, SPEED / 3)
+	velocity.y = move_toward(velocity.y, targetspeed.y, SPEED / 3)
 
 	move_and_slide()
