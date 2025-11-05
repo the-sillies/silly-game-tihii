3981c59e8c32a2c4ef049a762d705b800c8a7547
CatHat <liase810@gmail.com>
2025-11-05 16:37:10 +0100

    removed old attempt at game dialogue

--- scripts/npc.gd
+++ scripts/npc.gd
@@ -2,7 +2,5 @@ extends CharacterBody2D
 
 
 func _on_interact_detection_area_body_entered(body: Node2D) -> void:
-	print('ping')
 	if body.has_method('player'):
 		Dialogic.start('timeline')
-		print('pong')
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,28 +1,28 @@
 extends CharacterBody2D
 
+func player():
+	pass
 
 const SPEED = 100.0
-const JUMP_VELOCITY = -400.0
+
+var is_interacting = false
+
+var absolute_velocity := Vector2()
 
 
-func _physics_process(_delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-	#	velocity += get_gravity() * delta
-	# Handle jump.
-	#if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-	#	velocity.y = JUMP_VELOCITY
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction_x := Input.get_axis("ui_left", "ui_right")
-	var direction_y := Input.get_axis("ui_up", "ui_down")
+func _physics_process(delta: float) -> void:
+	var direction_x : float = 0
+	var direction_y : float = 0
+	
+	if not is_interacting:
+		direction_x = Input.get_axis("left", "right")
+		direction_y = Input.get_axis("up", "down")
 	
 	var targetspeed = {x = direction_x * SPEED, y = direction_y * SPEED}
 	
-	velocity.x = move_toward(velocity.x, targetspeed.x, SPEED / 3)
-	velocity.y = move_toward(velocity.y, targetspeed.y, SPEED / 3)
+	absolute_velocity.x = move_toward(absolute_velocity.x, targetspeed.x, SPEED / 5)
+	absolute_velocity.y = move_toward(absolute_velocity.y, targetspeed.y, SPEED / 5)
+	
+	velocity = absolute_velocity * delta * 60
 
 	move_and_slide()
-
-func player():
-	pass
