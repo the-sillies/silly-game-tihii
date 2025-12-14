788e5f754efeccee3b82200ff1005f608d81289d
CatHat <liase810@gmail.com>
2025-12-14 18:45:58 +0100

    make interaction tooltip appear on npcs

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 @export var logs: Label
 @export var sprite: AnimatedSprite2D
 @export var shadow: Sprite2D
+@export var interaction_tooltip: PackedScene
 
 @export var WALK_SPEED: float
 @export var RUN_SPEED: float
@@ -16,6 +17,8 @@ var interactable_nodes: Array[Node2D] = []
 var shadow_coords := Vector2()
 var shadow_coords_flipped := Vector2()
 
+var interaction_tooltip_instance
+
 func set_shadow_coords(x: float, y: float):
 	shadow_coords = Vector2(x, y)
 	shadow_coords_flipped = Vector2(-x, y)
@@ -25,6 +28,7 @@ func player():
 
 func _ready():
 	set_shadow_coords(1, 2)
+	interaction_tooltip_instance = interaction_tooltip.instantiate()
 
 func _physics_process(delta: float) -> void:
 	var direction_x : float = 0
@@ -81,6 +85,7 @@ func add_interactable(node: Node2D):
 func remove_interactable(node: Node2D):
 	var id := node.get_instance_id()
 	interactable_nodes = interactable_nodes.filter(func(item): return item.get_instance_id() != id)
+	node.remove_child(interaction_tooltip_instance)
 	print('removed: ', node)
 
 func sort_interactable_by_distance():
@@ -90,7 +95,11 @@ func update_closest_interactable():
 	var sorted: Array[Node2D] = []
 	sorted.assign(sort_interactable_by_distance())
 	interactable_nodes = sorted
-	pass
+	if interactable_nodes.size() > 0:
+		for node in interactable_nodes:
+			if node.get_children().has(interaction_tooltip_instance):
+				node.remove_child(interaction_tooltip_instance)
+		interactable_nodes[0].add_child(interaction_tooltip_instance)
 
 func _on_find_closest_timeout() -> void:
 	update_closest_interactable()
